4e0d3d3f41072908a8d60c9c95e06f1fbe8e63e1
User Pro <Nasiritwiz@gmail.com>
2026-03-02 18:40:21 +0000

    Runtime FirebaseError
    Firebase Storage: No default bucket found. Did y

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:931238246461:web:fe60c9ed66456564de5254",
   "apiKey": "AIzaSyAvshsvP_ZMZ2wg1ejvoDWOSksjcvFUF_s",
   "authDomain": "studio-9866611796-83af2.firebaseapp.com",
+  "storageBucket": "studio-9866611796-83af2.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "931238246461"
 };
